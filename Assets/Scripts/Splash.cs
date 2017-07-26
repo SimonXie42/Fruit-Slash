58c2b123deac5c21712795e9e040f242f2eabece
SimonXie42 <yuan.xie@mail.mcgill.ca>
2017-07-26 13:33:15 +0800

    Update Splash.cs

--- Assets/Scripts/Splash.cs
+++ Assets/Scripts/Splash.cs
@@ -3,18 +3,8 @@ using System.Collections;
 
 public class Splash : MonoBehaviour {
 
-    private float time;
-
 	// Use this for initialization
 	void Start () {
-        time = Time.time;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Time.time - time > 2f)
-        {
-            Destroy(gameObject);        // Remove Splash 2s after it appears
-        }
+		Destroy(gameObject, 2f);        // Remove Splash 2s after it appears
 	}
 }
