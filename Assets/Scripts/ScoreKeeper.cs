01e25bbc6223498cc61a2cb16e83440507dfe667
SimonXie42 <yuan.xie@mail.mcgill.ca>
2017-07-28 16:00:01 +0800

    Update ScoreKeeper.cs

--- Assets/Scripts/ScoreKeeper.cs
+++ Assets/Scripts/ScoreKeeper.cs
@@ -6,15 +6,15 @@ public class ScoreKeeper : MonoBehaviour {
     public int Score = 0;
     public ScoreDisplay sd;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         sd = FindObjectOfType<ScoreDisplay>();
     }
 
     // Update is called once per frame
     void Update () {
         sd.text.text = Score.ToString();
-	}
+    }
 
     public void RecordHighScore()
     {
